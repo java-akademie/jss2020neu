fd9b7c1aaf4aab15d52082ebf8521e0a4ca864e9
Johann Mildner <johann.mildner@balcab.ch>
2020-06-23 17:30:55 +0200

    jmb

--- _js/myfunctions.js
+++ _js/myfunctions.js
@@ -1,9 +1,20 @@
-
 /* jshint esversion: 6 */
+
+/**
+ * nur waehrend der Entwicklungsphase
+ */
+let fehler = "";
+let count = 0;
+window.onerror = function(message, source, lineno, colno, error) {
+    count++;
+    fehler = fehler + count + ". Fehler: <br />" + message + " at <br />" +
+        source + "<br />lineno " + lineno + "<br /><br />";
+    document.getElementById("error").innerHTML = fehler;
+};
+
 
 function quadrat() {
     const zahl = +window.prompt("bitte geben Sie die Zahl ein");
     const erg = zahl * zahl;
     window.alert(zahl + " hoch 2 ergibt " + erg + "!");
 }
-
